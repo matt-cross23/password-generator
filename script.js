2140550125ddcca3dc1f55579c258c7fb8e0d078
Matthew Cross <matthewcross1017@gmail.com>
2021-09-22 18:04:15 -0400

    Update JS

---
+++ script.js
@@ -0,0 +1,73 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseLetters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialCharacters = ["!","@","#","$","%","^","&","*","(",")","_","+","[","]","{","}",";",":",];
+var Numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var criteria = [];
+var preference = [];
+
+// console.log ("This a test for the array " + lowercaseLetters.length); 
+passwordLength = true
+
+function writePassword (){
+passwordLength = prompt("Please input the number of characters for your password(8 < X > 128");
+{if (passwordLength) {passwordLength < 8 || passwordLength > 128};
+else (!passwordLength); {alert ("Incorrect Value")};
+}
+
+// Write password to the #password input needs a prompt function
+
+  function writePassword() { 
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+// Password Criteria
+  
+var confirmUpper = confirm("Press okay to include uppercase letters");
+var confirmLower = confirm("Press okay to include lowercase letters");
+var confirmNumber = confirm("Press okay to include numbers");
+var confirmSpecial = confirm("Press okay to include special characters");
+
+// Generate Criteria based on user
+// Empty Array for User's input
+
+var userInput = [];
+ 
+  function passwordGenerator (){
+  return Math.floor(Math.random(array.length) * 10);
+}
+      
+// Add event listener to generate button
+
+generateBtn.addEventListener("click", writePassword);
+
+// document.getElementbyId("password")
+// var x = document.getElementById("myBtn").textContent);{
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// if length of password is >= 8 and <= 128,
+// the password is valid
+// else
+// the password is invalid
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// var lowercase = true/false???
+// var uppercase = true/false???
+// var numeric = true/false???
+// var special = true/false???
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
+// alert("Password is: ....");
+
+// Add event listener to generate button}
